b20b83f503b0b7c1cd762a47538b5d349c15677c
Aryan <aryanrathore040@gmail.com>
2025-02-24 12:55:07 +0530

    Created sample dotenv

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express=require('express');
 const app=express();
 
+
+const {PORT}= require('./config/serverConfig');
+
 app.listen(3000,()=>{
-    console.log("Server for shopcart is up");
+    console.log(`Server for shopcart is up on ${PORT}`);
 })
