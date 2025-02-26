e7ebe4b3cf930b2708b2213c40bb8b235be29b71
Aryan <aryanrathore040@gmail.com>
2025-02-26 12:21:30 +0530

    demo for middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const express=require('express');
 const app=express();
-
+const responsetime=require('response-time');
+const bodyparser=require('body-parser');
 
 const {PORT}= require('./config/serverConfig');
 const {pingConfigRoutes} = require('./routes/v1/pingRouter')
 const apiRouter=require('./routes/apiRouter');
-
+// app.use(responsetime(function f(request,response,time){
+//     console.log("Time elapsed",time);
+// }));
+app.use(responsetime());
+app.use(bodyparser.json());
+app.use(bodyparser.text());
+app.use(bodyparser.urlencoded({extended:true}));
 app.use('/api',apiRouter);
 app.listen(PORT,()=>{
     console.log(`Server for shopcart is up on ${PORT}`);
--- src/routes/v1/pingRouter.js
+++ src/routes/v1/pingRouter.js
@@ -1,6 +1,20 @@
 const {pingController }= require('../../controllers/pingController');
 const express=require('express');
 const router=express.Router();
-router.get('/ping',pingController);
+
+function checkrequest(request,response,next){
+    console.log("Request is getting checked");
+    next();
+}
+
+function logger(request,response,next){
+    console.log("Logging the request");
+    next();
+}
+function authchecker(request,response,next){
+    console.log("Request is authenticated");
+    next();
+}
+router.get('/',[checkrequest,logger,authchecker],pingController);
 
 module.exports = router;
