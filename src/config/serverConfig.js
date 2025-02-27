d952dd1fb52d1564794785a6f846fbceae8b296c
Aryan <aryanrathore040@gmail.com>
2025-02-27 17:45:57 +0530

    connected to mysql and implemented query

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -3,5 +3,9 @@ const dotenv=require('dotenv');
 dotenv.config();
 
 module.exports={
-    PORT:process.env.PORT || 3000
+    PORT:process.env.PORT || 3000,
+    DB_Host:process.env.DB_Host,
+    DB_USER:process.env.DB_USER,
+    DB_PASS:process.env.DB_PASS,
+    DB_NAME:process.env.DB_NAME,
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ const express=require('express');
 const app=express();
 const responsetime=require('response-time');
 const bodyparser=require('body-parser');
-
-const {PORT}= require('./config/serverConfig');
+const mysql=require('mysql2');
+const {PORT,DB_Host,DB_NAME,DB_PASS,DB_USER}= require('./config/serverConfig');
 const {pingConfigRoutes} = require('./routes/v1/pingRouter')
+var db = mysql.createConnection({
+    host:DB_Host,
+    user:DB_USER,
+    password: DB_PASS,
+    database: DB_NAME
+})
 const apiRouter=require('./routes/apiRouter');
 // app.use(responsetime(function f(request,response,time){
 //     console.log("Time elapsed",time);
@@ -16,4 +22,18 @@ app.use(bodyparser.urlencoded({extended:true}));
 app.use('/api',apiRouter);
 app.listen(PORT,()=>{
     console.log(`Server for shopcart is up on ${PORT}`);
+    db.connect((err)=>{
+        if(err){
+            console.log("DB didnt connect");
+            console.log(err);
+            throw err;
+        }
+        console.log("Conected boss");
+        db.query(`Select * from productstb`,(err,result)=>{
+            if(err){
+                throw err;
+            }
+            console.log(result);
+        })
+    });
 })
