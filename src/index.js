9d24e11e8df881983067209ede00ba4b6cd3371c
Aryan <aryanrathore040@gmail.com>
2025-02-28 13:04:16 +0530

    created a table in our db using sequelize define

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const bodyparser=require('body-parser');
 const apiRouter=require('./routes/apiRouter');
 const db =require('./config/db_config');
 const {PORT} = require('./config/serverConfig')
+const category=require('../src/models/category')
 // app.use(responsetime(function f(request,response,time){
 //     console.log("Time elapsed",time);
 // }));
@@ -17,4 +18,10 @@ app.listen(PORT,async ()=>{
     console.log(`Server for shopcart is up on ${PORT}`);
     await db.sync();
     console.log('db connected')
+    // const res=await category.create({
+    //     name:'Electronics',
+    //     description:'Cateogory for Electronic Products'
+    // });
+    // console.log(res);
+    
 })
